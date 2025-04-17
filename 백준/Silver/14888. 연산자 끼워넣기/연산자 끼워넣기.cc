34beb2d690af62063ff52838e9ac7210b272d51f
Hayoung <76741020+Hayeong1224@users.noreply.github.com>
2025-04-17 22:54:46 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int A[11], O[4];
+int N, minSum = 1000000000, maxSum = -1000000000;
+
+void dfs(int num, int sum) {
+    // 끝나는 경우
+    if (num == N) {
+        if (sum < minSum) minSum = sum;
+        if (sum > maxSum) maxSum = sum;
+        return;
+    }
+
+    // 각 단계별 할 일
+    for (int i = 0; i < 4; i++) {
+        if (O[i] > 0) {
+            O[i]--;
+            if (i == 0) dfs(num + 1, sum + A[num]);
+            else if (i == 1) dfs(num + 1, sum - A[num]);
+            else if (i == 2) dfs(num + 1, sum * A[num]);
+            else dfs(num + 1, sum / A[num]);
+            O[i]++;
+        }
+    }
+    return;
+}
+
+int main() {
+    cin >> N;
+    for (int i = 0; i < N; i++) cin >> A[i];
+    for (int i = 0; i < 4; i++) cin >> O[i];
+
+    dfs(1, A[0]);
+    cout << maxSum << '\n' << minSum;
+}
