7a36dd006adef6b904b48f48d5793404102bd426
Hayoung <76741020+Hayeong1224@users.noreply.github.com>
2025-11-23 21:47:47 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -2,23 +2,21 @@ def solution(n, lost, reserve):
     lost.sort()
     reserve.sort()
     
-    new = []
-    # 자기자신 제외 (lost에서 삭제하면 한 칸씩 앞으로 와서 삭제가 제대로 안 됨 -> 중복이 아니면 new에 넣기)
-    for x in lost:
-        if x in reserve:
-            reserve.remove(x)
-        else:
-            new.append(x)
-    
-    answer = n - len(new)
-            
+    # 자기자신 제외
+    _lost = [l for l in lost if l not in reserve]
+    _reserve = [r for r in reserve if r not in lost]
+     
+    answer = n - len(_lost)
     # 앞뒤에서 빌리는 거 제외
-    for x in new:
-        if reserve.count(x-1) > 0:
-            reserve.remove(x-1)
+    for x in _lost:
+        f = x - 1
+        b = x + 1
+        
+        if f in _reserve:
+            _reserve.remove(f)
             answer += 1
-        elif reserve.count(x+1) > 0:
-            reserve.remove(x+1)
-            answer += 1    
+        elif b in _reserve:
+            _reserve.remove(b)
+            answer += 1
     
     return answer
