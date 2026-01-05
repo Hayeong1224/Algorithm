1458b6aa50cc05f45a388073f1d90a33bfa0fb41
Hayoung <76741020+Hayeong1224@users.noreply.github.com>
2026-01-05 23:13:42 +0900

    [level 3] Title: 입국심사, Time: 578.55 ms, Memory: 13.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -0,0 +1,16 @@
+def solution(n, times):
+    start = min(times)
+    end = max(times) * n
+    answer = 0
+    while(start <= end):
+        sum = 0
+        mid = (start + end) // 2
+        for x in times:
+            sum += mid // x
+        if sum < n :
+            start = mid + 1
+        else:
+            answer = mid
+            end = mid - 1
+            
+    return answer
