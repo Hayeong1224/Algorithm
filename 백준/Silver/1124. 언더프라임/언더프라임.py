c5cdcfab4c1b1c83675eaa0b4670416edd1c1e66
Hayoung <76741020+Hayeong1224@users.noreply.github.com>
2026-01-12 22:39:15 +0900

    [Silver I] Title: 언더프라임, Time: 140 ms, Memory: 35784 KB -BaekjoonHub

---
+++ 백준/Silver/1124. 언더프라임/언더프라임.py
@@ -0,0 +1,37 @@
+import sys
+input = sys.stdin.readline
+
+a, b = map(int, input().split())
+
+MAX = 100001
+is_prime = [True] * MAX
+is_prime[0] = is_prime[1] = False
+
+# 소수 판별
+for i in range(2,int(MAX**0.5)+1):
+  if is_prime[i]:
+    for j in range(i*i,MAX,i):
+      if is_prime[j]:
+        is_prime[j] = False
+
+# 소인수 개수 DP -> f(n) = f(n/p) + 1
+# 1. 가장 작은 소인수 찾기
+min_prime = [0] * MAX
+for i in range(2,MAX):
+  if min_prime[i] == 0:
+    for j in range(i,MAX,i):
+      if min_prime[j] == 0:
+        min_prime[j] = i
+
+# 2. 소인수 개수 찾기
+f_count = [0] * MAX
+for i in range(2,MAX):
+  f_count[i] = f_count[i // min_prime[i]] + 1
+        
+# 최종 - 언더프라임인지 판별
+count = 0
+for i in range(a,b+1):
+  if is_prime[f_count[i]]:
+    count += 1
+
+print(count)
