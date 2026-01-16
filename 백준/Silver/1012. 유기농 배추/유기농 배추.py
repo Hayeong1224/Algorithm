f9d77f9318a1bb63443bed82d55df04a09fbd96b
Hayoung <76741020+Hayeong1224@users.noreply.github.com>
2026-01-16 21:35:05 +0900

    [Silver II] Title: 유기농 배추, Time: 220 ms, Memory: 34976 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,14 +1,10 @@
 from collections import deque
 T = int(input())
-field = [[0] * 50 for _ in range(50)]
 
 dx = [1,0,-1,0]
 dy = [0,-1,0,1]
 
 def bfs(x, y):
-  if field[x][y] == 0:
-    return 0
-  
   q = deque([(x,y)])
   field[x][y] = 0
   while q:
@@ -24,6 +20,7 @@ def bfs(x, y):
 
 for _ in range(T):
   m, n, k = map(int, input().split())
+  field = [[0] * 50 for _ in range(50)]
   for _ in range(k):
     x, y = map(int, input().split())
     field[x][y] = 1
@@ -31,6 +28,7 @@ for _ in range(T):
   count = 0
   for i in range(m):
     for j in range(n):
-      count += bfs(i,j)
+      if field[i][j] == 1:
+        count += bfs(i,j)
 
   print(count)
