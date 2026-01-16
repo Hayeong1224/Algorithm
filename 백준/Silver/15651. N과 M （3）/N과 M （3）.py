2b684ccab8e437bb30800e657dfa1797c2e4fdfb
Hayoung <76741020+Hayeong1224@users.noreply.github.com>
2026-01-16 22:19:19 +0900

    [Silver III] Title: N과 M (3), Time: 1884 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,14 @@
+n, m = map(int, input().split())
+result = []
+
+def dfs(num):
+  if len(result) == m:
+    print(*(result))
+    return
+
+  for i in range(1,n+1):
+    result.append(i)
+    dfs(i)
+    result.pop()
+
+dfs(1)
