e1348d81506fd3860edd158b2369a9de30ddadfd
Hayoung <76741020+Hayeong1224@users.noreply.github.com>
2026-01-18 17:11:04 +0900

    [Silver V] Title: 집합, Time: 2624 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/11723. 집합/집합.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+all_elements = [str(i) for i in range(1,21)]
+
+m = int(input())
+s = set()
+
+for _ in range(m):
+  line = input().split()
+  cmd = line[0]
+  
+  if cmd == "add":
+    s.add(line[1])
+
+  elif cmd == "remove":
+    s.discard(line[1])
+
+  elif cmd == "check":
+    print(1 if line[1] in s else 0)
+
+  elif cmd == "toggle":
+    s.remove(line[1]) if line[1] in s else s.add(line[1])
+
+  elif cmd == "all":
+    s.update(all_elements)
+      
+  else:
+    s.clear()
