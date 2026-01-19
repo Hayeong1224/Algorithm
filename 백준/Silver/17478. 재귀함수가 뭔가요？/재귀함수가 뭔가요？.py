28e6182755864992d3f2f9c3709341e86fbf2bbc
Hayoung <76741020+Hayeong1224@users.noreply.github.com>
2026-01-20 00:27:20 +0900

    [Silver V] Title: 재귀함수가 뭔가요?, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.py
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.py
@@ -1,27 +1,22 @@
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10**6)
 
-def dfs(count):
-  print("____" * count, end ='')
-  print('"재귀함수가 뭔가요?"')
+def dfs(count, n):
+  recursion_num = "____" * count
+  print(f'{recursion_num}"재귀함수가 뭔가요?"')
   if count == n:
-    print("____" * count, end ='')
-    print('"재귀함수는 자기 자신을 호출하는 함수라네"')
+    print(f'{recursion_num}"재귀함수는 자기 자신을 호출하는 함수라네"')
 
   else:
-    print("____" * count, end ='')
-    print('"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.')
-    print("____" * count, end ='')
-    print('마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.')
-    print("____" * count, end ='')
-    print('그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어."')
-    dfs(count+1)
+    print(f'{recursion_num}"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.')
+    print(f'{recursion_num}마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.')
+    print(f'{recursion_num}그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어."')
     
-  print("____" * count, end ='')
-  print('라고 답변하였지.')
+    dfs(count+1, n)
+    
+  print(f'{recursion_num}라고 답변하였지.')
   
 
 n = int(input())
 print("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.")
-dfs(0)
+dfs(0, n)
