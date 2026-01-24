904be666f81c7606e62716b91919d9c25b361efd
Hayoung <76741020+Hayeong1224@users.noreply.github.com>
2026-01-24 18:25:00 +0900

    [Silver V] Title: 임스와 함께하는 미니게임, Time: 2444 ms, Memory: 43680 KB -BaekjoonHub

--- 백준/Silver/25757. 임스와 함께하는 미니게임/임스와 함께하는 미니게임.py
+++ 백준/Silver/25757. 임스와 함께하는 미니게임/임스와 함께하는 미니게임.py
@@ -3,10 +3,4 @@ n = int(n)
 players = {input() for _ in range(n)}
 min_p = {'Y': 1, 'F': 2, 'O': 3} # 임스 제외
 
-count = 0
-while len(players) >= min_p[g]:
-  for i in range(min_p[g]):
-    players.pop()
-  count += 1
-
-print(count)
+print(len(players)//min_p[g])
