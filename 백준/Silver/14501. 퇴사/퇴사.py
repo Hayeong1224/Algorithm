f086da0b48426ef0e5f1e8510dd08d8153786a5f
Hayoung <76741020+Hayeong1224@users.noreply.github.com>
2026-01-24 16:50:54 +0900

    [Silver III] Title: 퇴사, Time: 116 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -10,18 +10,16 @@ for i in range(n): # 불가능한 상담(n-1일 이후) 빼고 받기
 ans = 0
 for i in range(1,len(counsels)+1):  
   for com in combinations(counsels, i):
-    temp = 0
-    if i != 1:
-      for j in range(i-1):
-        d1,t1,p1 = com[j]
-        d2,t2,p2 = com[j+1]
+    possible = True
+    for j in range(i-1):
+        d1,t1,_ = com[j]
+        d2,_,_ = com[j+1]
+
         if d1+t1 > d2:
-          temp = 0
+          possible = False
           break
-      
-        temp += (p1+p2) if j == i-2 else p1
-    else:
-      temp += com[0][2]
-    ans = max(ans, temp)
+
+    if possible:
+      ans = max(ans, sum(c[2] for c in com))
 
 print(ans)
