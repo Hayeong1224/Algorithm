c016185421baa0151680b0ebc63ea0de712b0b9a
Hayoung <76741020+Hayeong1224@users.noreply.github.com>
2026-01-25 16:38:00 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,4 +1,4 @@
-def dfs(n, temp, op_list):
+def dfs(n, temp, p, m, mu, d):
   global min_ans, max_ans
   
   if n == N:
@@ -6,31 +6,23 @@ def dfs(n, temp, op_list):
     max_ans = max(max_ans, temp)
     return
 
-  if op_list[0] > 0:
-    new_op = op_list.copy()
-    new_op[0] -= 1
-    dfs(n+1, temp+array[n], new_op)
+  if p > 0:
+    dfs(n+1, temp+array[n], p-1, m, mu, d)
 
-  if op_list[1] > 0:
-    new_op = op_list.copy()
-    new_op[1] -= 1
-    dfs(n+1, temp-array[n], new_op)
+  if m > 0:
+    dfs(n+1, temp-array[n], p, m-1, mu, d)
 
-  if op_list[2] > 0:
-    new_op = op_list.copy()
-    new_op[2] -= 1
-    dfs(n+1, temp*array[n], new_op)
+  if mu > 0:
+    dfs(n+1, temp*array[n], p, m, mu-1, d)
 
-  if op_list[3] > 0:
-    new_op = op_list.copy()
-    new_op[3] -= 1
-    dfs(n+1, int(temp/array[n]), new_op)
+  if d > 0:
+    dfs(n+1, int(temp/array[n]), p, m, mu, d-1)
 
 
 N = int(input())
 array = list(map(int, input().split()))
-op = list(map(int, input().split()))
+p, m, mu, d = map(int, input().split())
       
 min_ans, max_ans = float('inf'), -float('inf')
-dfs(1,array[0],op)
+dfs(1,array[0], p, m, mu, d)
 print(max_ans, min_ans, sep='\n')
