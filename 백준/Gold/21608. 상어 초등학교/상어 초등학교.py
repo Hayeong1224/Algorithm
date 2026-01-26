0f6aaa0f91cda053cd7cf3822e48b3db6d17a2f0
Hayoung <76741020+Hayeong1224@users.noreply.github.com>
2026-01-26 19:00:42 +0900

    [Gold V] Title: 상어 초등학교, Time: 228 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/21608. 상어 초등학교/상어 초등학교.py
+++ 백준/Gold/21608. 상어 초등학교/상어 초등학교.py
@@ -1,46 +1,49 @@
 N = int(input())
-# 0: 자기 번호, 1/2/3/4: 좋아하는 학생 번호
-students = [list(map(int, input().split())) for _ in range(N**2)]
-board = [[0] * N for _ in range(N)]
+student_info = []
+like_dict = {}
 
+for _ in range(N**2):
+  line = list(map(int, input().split()))
+  student_info.append(line[0])
+  like_dict[line[0]] = set(line[1:])
+
+board = [[0] * N for _ in range(N)]
 dx = [-1,1,0,0]
 dy = [0,0,-1,1]
 
-for i in range(N**2):
-  max_f, max_status = 0, []
+for s_id in student_info:
+  candidates = []
   for r in range(N):
     for c in range(N):
       if board[r][c] != 0:
         continue
-      # 인접한 좋아하는 학생 수, 인접 빈 칸 수 세기
-      favorites, empty_seats = 0, 0
+
+      likes, empties = 0, 0
       for k in range(4):
         nr, nc = r + dx[k], c + dy[k]
         if 0<=nr<N and 0<=nc<N:
           if board[nr][nc] == 0:
-            empty_seats += 1
-          elif board[nr][nc] in students[i][1:]:
-            favorites += 1
-      if max_f == favorites:
-        max_status.append([r, c, empty_seats])
-      elif max_f < favorites:
-        max_f, max_status = favorites, [[r, c, empty_seats]]
+            empties += 1
+          elif board[nr][nc] in like_dict[s_id]:
+            likes += 1
+      candidates.append((-likes, -empties, r, c))
 
-  max_status.sort(key= lambda x : (-x[2],x[0],x[1]))
-  board[max_status[0][0]][max_status[0][1]] = students[i][0]
+  candidates.sort()
+  _, _, best_r, best_c = candidates[0]
+  board[best_r][best_c] = s_id
 
 ans = 0
+score = {0: 0, 1: 1, 2: 10, 3: 100, 4: 1000}
 
-for i in range(N**2):
-  x,y = next((r,c) for r in range(N) for c in range(N) if board[r][c] == students[i][0])
+for r in range(N):
+  for c in range(N):
+    s_id = board[r][c]
+    cnt = 0
+    for k in range(4):
+      nr, nc = r + dx[k], c + dy[k]
+      if 0<=nr<N and 0<=nc<N:
+        if board[nr][nc] in like_dict[s_id]:
+          cnt += 1
 
-  favorites = 0
-  for k in range(4):
-    nx, ny = x + dx[k], y + dy[k]
-    if 0<=nx<N and 0<=ny<N:
-      if board[nx][ny] in students[i][1:]:
-        favorites += 1
-
-  ans += 10 ** (favorites-1) if favorites > 0 else 0
-
+    ans += score[cnt]
 print(ans)
