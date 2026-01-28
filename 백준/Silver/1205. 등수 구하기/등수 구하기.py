a6881056aed13c727a6b22b696f87d0a137cf3e6
Hayoung <76741020+Hayeong1224@users.noreply.github.com>
2026-01-28 23:57:18 +0900

    [Silver IV] Title: 등수 구하기, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Silver/1205. 등수 구하기/등수 구하기.py
@@ -0,0 +1,18 @@
+N, new_score, P = map(int, input().split())
+
+if N == 0:
+  print(1)
+  exit()
+
+scores = list(map(int, input().split()))  
+
+if N >= P and new_score <= scores[-1]:
+    print(-1)
+    exit()
+    
+rank = 1
+for s in scores:
+  if new_score < s:
+    rank += 1
+
+print(rank if rank <= P else -1)
