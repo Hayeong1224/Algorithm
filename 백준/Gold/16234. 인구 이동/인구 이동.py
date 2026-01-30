4dbddf4a316df7e602d6d65cdba084b33380da22
Hayoung <76741020+Hayeong1224@users.noreply.github.com>
2026-01-31 00:00:32 +0900

    [Gold IV] Title: 인구 이동, Time: 1284 ms, Memory: 131844 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.py
+++ 백준/Gold/16234. 인구 이동/인구 이동.py
@@ -3,22 +3,28 @@ from collections import deque
 input = sys.stdin.readline
 
 def bfs(r, c):
-  global unions, visited
-  union = []
+  global visited
+  union = [(r,c)]
   q = deque([(r,c)])
   visited[r][c] = True
-  union.append((r,c))
+  total_population = A[r][c]
+
   while q:
     x, y = q.popleft()
     for i in range(4):
       nx, ny = x + dx[i], y + dy[i]
       if 0 <= nx < N and 0 <= ny < N and not visited[nx][ny] and L <=  abs(A[x][y] - A[nx][ny]) <= R:
-        q.append((nx,ny))
         visited[nx][ny] = True
+        q.append((nx,ny))
         union.append((nx,ny))
+        total_population += A[nx][ny]
 
-  unions.append(union)
-
+  if len(union) > 1:
+    avg = total_population // len(union)
+    for x, y in union:
+      A[x][y] = avg
+    return True
+  return False
 
 N, L, R = map(int, input().split())
 A = [list(map(int, input().split())) for _ in range(N)]
@@ -28,21 +34,17 @@ dy = [0,1,0,-1]
 
 ans = 0
 while True:
-  unions = list() # unions[i] = [(r,c), (r2,c2), ...]
   visited = [[False] * N for _ in range(N)]
+  moved = False
+  
   for r in range(N):
     for c in range(N):
       if not visited[r][c]:
-        bfs(r,c)
+        if bfs(r,c):
+          moved = True
 
-  if len(unions) == N*N:
-    print(ans)
+  if not moved:
     break
-  else:
-    ans += 1
-    # 인구 이동
-    for union in unions:
-      n = len(union)
-      total = sum(A[x][y] for x, y in union)
-      for x, y in union:
-        A[x][y] = total // n
+  ans += 1
+
+print(ans)
