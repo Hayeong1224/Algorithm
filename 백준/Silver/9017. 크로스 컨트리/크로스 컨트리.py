940b5f6e1e3c06724e458800f04b8451c255a817
Hayoung <76741020+Hayeong1224@users.noreply.github.com>
2026-02-08 23:41:09 +0900

    [Silver III] Title: 크로스 컨트리, Time: 124 ms, Memory: 110976 KB -BaekjoonHub

---
+++ 백준/Silver/9017. 크로스 컨트리/크로스 컨트리.py
@@ -0,0 +1,24 @@
+T = int(input())
+for _ in range(T):
+  N = int(input())
+  ranking = list(map(int, input().split()))
+  teams = {}
+
+  cnt = 0
+  for rank, t in enumerate(ranking):
+    if ranking.count(t) < 6:
+      cnt += 1
+      continue
+
+    if t not in teams:
+      teams[t] = [rank+1-cnt]
+    else:
+      teams[t].append(rank+1-cnt)
+
+  winner, winner_score = -1, float('inf')
+  for t in teams.keys():
+    team_score = sum(teams[t][:4])
+    if team_score < winner_score or (team_score == winner_score and teams[winner][4] > teams[t][4]):
+      winner, winner_score = t, team_score
+
+  print(winner)
