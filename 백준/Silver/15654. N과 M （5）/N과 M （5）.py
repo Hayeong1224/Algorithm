53423bf2f3e10a66d6cdf7f0e58b253522ab0a1b
Hayoung <76741020+Hayeong1224@users.noreply.github.com>
2026-02-15 22:48:36 +0900

    [Silver III] Title: N과 M (5), Time: 152 ms, Memory: 118320 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -0,0 +1,20 @@
+def dfs(seq, m):
+  if m == 0:
+    ans.append(list(seq))
+    return
+
+  for a in array:
+    if a not in seq:
+      seq.append(a)
+      dfs(seq, m-1)
+      seq.remove(a)
+  return
+
+n, m = map(int, input().split())
+array = list(map(int, input().split()))
+array.sort()
+ans = list()
+
+dfs([], m)
+for seq in ans:
+  print(*seq)
