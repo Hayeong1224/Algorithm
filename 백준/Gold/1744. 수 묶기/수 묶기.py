795de211086f44a281626597cab9c9ff25c28f95
Hayoung <76741020+Hayeong1224@users.noreply.github.com>
2026-02-28 01:07:13 +0900

    [Gold IV] Title: 수 묶기, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Gold/1744. 수 묶기/수 묶기.py
+++ 백준/Gold/1744. 수 묶기/수 묶기.py
@@ -1,30 +1,34 @@
 n = int(input())
 arr = [int(input()) for _ in range(n)]
 
-if n == 1:
-  print(arr[0])
+pos, neg, ones, zeros = [], [], 0, 0
 
-else:
-  arr_pos = sorted([num for num in arr if num > 1], reverse = True)
-  sum_one = arr.count(1)
-  arr_zero = [num for num in arr if num == 0]
-  arr_neg = sorted([num for num in arr if num < 0])
-  
-  result = sum_one # 1은 다 더하고 시작
-  
-  if arr_pos: # 양수
-    if len(arr_pos) % 2 == 0: # 짝수개
-      result += sum([arr_pos[i] * arr_pos[i+1] for i in range(0, len(arr_pos), 2)])
-    else: # 홀수개
-      result += sum([arr_pos[i] * arr_pos[i+1] for i in range(0, len(arr_pos)-1, 2)])
-      result += arr_pos[-1]
+for num in arr:
+  if num > 1:
+    pos.append(num)
+  elif num == 1:
+    ones += 1
+  elif num == 0:
+    zeros += 1
+  else:
+    neg.append(num)
 
-  if arr_neg: # 음수
-    if len(arr_neg) % 2 == 0: # 짝수개
-      result += sum([arr_neg[i] * arr_neg[i+1] for i in range(0, len(arr_neg), 2)])
-    else: # 홀수개
-      result += sum([arr_neg[i] * arr_neg[i+1] for i in range(0, len(arr_neg)-1, 2)])
-      if not arr_zero:
-        result += arr_neg[-1]
-  
-  print(result)
+pos.sort(reverse=True)
+neg.sort()
+
+result = ones # 1은 다 더하고 시작
+
+# 양수 묶기
+for i in range(0, len(pos) - 1, 2):
+  result += pos[i] * pos[i+1]
+if len(pos) % 2 == 1:
+  result += pos[-1]
+
+# 음수 묶기
+for i in range(0, len(neg) - 1, 2):
+  result += neg[i] * neg[i+1]
+if len(neg) % 2 == 1:
+  if zeros == 0:
+    result += neg[-1]
+
+print(result)
