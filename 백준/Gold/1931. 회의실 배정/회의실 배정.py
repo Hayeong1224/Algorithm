8e0c07193afd58b8143e8781a6fc0a5d6425bf0b
Hayoung <76741020+Hayeong1224@users.noreply.github.com>
2026-02-28 23:10:34 +0900

    [Gold V] Title: 회의실 배정, Time: 364 ms, Memory: 128368 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.py
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.py
@@ -1,15 +1,10 @@
-N = int(input())
-meetings = [list(map(int, input().split())) for i in range(N)]
+n = int(input())
+meetings = sorted([list(map(int, input().split())) for _ in range(n)], key= lambda x : (x[1], x[0])) # [start, end] end 기준으로 정렬
 
-# 빨리 끝나는 기준으로 정렬
-meetings.sort(key = lambda x: (x[1], x[0]))
-
-count = 0
-last_end_time = 0
-
+cnt, prev = 0, 0 
 for start, end in meetings:
-  if start >= last_end_time:
-    count += 1
-    last_end_time = end
+  if start >= prev:
+    cnt += 1
+    prev = end
 
-print(count)
+print(cnt)
