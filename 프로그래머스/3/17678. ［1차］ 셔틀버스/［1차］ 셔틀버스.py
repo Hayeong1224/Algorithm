71b0c6f6594f4dbf42aa24b26b476b210956a1b9
Hayoung <76741020+Hayeong1224@users.noreply.github.com>
2026-03-04 00:57:08 +0900

    [level 3] Title: [1차] 셔틀버스, Time: 0.95 ms, Memory: 9.54 MB -BaekjoonHub

--- 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.py
+++ 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.py
@@ -1,49 +1,21 @@
 from collections import deque
 
 def solution(n, t, m, timetable):
-    shuttle = [(9,0)]
-    lh, lm = 9, 0
-    for _ in range(n - 1):
-        lm += t
-        if lm >= 60:
-            lh += 1
-            lm -= 60
-        shuttle.append((lh,lm))
+    # 크루들 시간 
+    crews = deque(sorted([int(tm[:2]) * 60 + int(tm[3:]) for tm in timetable]))
     
-    new_t = []
-    for t in timetable:
-        th, tm = map(int, t.split(':'))
-        if th < lh or (th == lh and tm <= lm):
-            new_t.append((th,tm))
-    new_t.sort(key = lambda x : (x[0], x[1]))
-    new_t = deque(new_t)
+    # 셔틀 버스 시간표
+    shuttle_times = [540 + i * t for i in range(n)]
     
-    if not new_t: # 탈 사람 없으면 마지막 셔틀 타기
-        ans = f'{lh:02}:{lm:02}'
-        return ans
-    
-    for i in range(len(shuttle)):
-        sh, sm = shuttle[i]
+    for i, st in enumerate(shuttle_times):
         cnt = 0
-        while new_t and cnt < m:
-            ch, cm = new_t.popleft()
-            if sh < ch or (sh == ch and sm < cm): # 크루가 셔틀보다 늦는 경우 제외
-                new_t.appendleft((ch,cm))
-                break
+        while crews and cnt < m and crews[0] <= st:
+            ct = crews.popleft()
             cnt += 1
-            if i == len(shuttle) - 1 and cnt == m: # 마지막 크루라면 1분 전에 와서 자리 뺏기
-                cm -= 1
-                if cm < 0:
-                    ch -= 1
-                    cm += 60
-                ans = f'{ch:02}:{cm:02}'
-                return ans
             
-        if i == len(shuttle) - 1: # 마지막 셔틀이 다 안 찼으면 셔틀 시간에 와도 됨!
-            ans = ans = f'{sh:02}:{sm:02}'
-            return ans
+            if i == n - 1 and cnt == m: # 마지막 크루라면 1분 전에 와서 자리 뺏기
+                ct -= 1
+                return f"{ct // 60:02}:{ct % 60:02}"
             
-        
-        
-    
-    
+        if i == n - 1: # 마지막 셔틀이 다 안 찼으면 셔틀 시간에 와도 됨
+            return f"{st // 60:02}:{st % 60:02}"
