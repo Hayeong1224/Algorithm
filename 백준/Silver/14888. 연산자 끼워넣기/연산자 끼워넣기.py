f1f0cc617a2e91204e1a69a043a992bbddb5458d
Hayoung <76741020+Hayeong1224@users.noreply.github.com>
2026-03-05 22:48:13 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 160 ms, Memory: 112088 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,28 +1,27 @@
-def dfs(n, temp, p, m, mu, d):
-  global min_ans, max_ans
-  
-  if n == N:
-    min_ans = min(min_ans, temp)
-    max_ans = max(max_ans, temp)
+def dfs(i, s, op):
+  global max_v, min_v
+  if i == N:
+    max_v = max(max_v, s)
+    min_v = min(min_v, s)
     return
 
-  if p > 0:
-    dfs(n+1, temp+array[n], p-1, m, mu, d)
-
-  if m > 0:
-    dfs(n+1, temp-array[n], p, m-1, mu, d)
-
-  if mu > 0:
-    dfs(n+1, temp*array[n], p, m, mu-1, d)
-
-  if d > 0:
-    dfs(n+1, int(temp/array[n]), p, m, mu, d-1)
-
+  for d in range(4):
+    if op[d] > 0:
+      op[d] -= 1
+      
+      if d == 0: dfs(i+1, s + A[i], op)
+      elif d == 1: dfs(i+1, s - A[i], op)
+      elif d == 2: dfs(i+1, s * A[i], op)
+      # 음수를 양수로 나눌 때는 C++14의 기준을 따른다. 
+      else: dfs(i+1, int(s / A[i]), op)
+      
+      op[d] += 1
 
 N = int(input())
-array = list(map(int, input().split()))
-p, m, mu, d = map(int, input().split())
-      
-min_ans, max_ans = float('inf'), -float('inf')
-dfs(1,array[0], p, m, mu, d)
-print(max_ans, min_ans, sep='\n')
+A = list(map(int, input().split()))
+op = list(map(int, input().split())) # +, -, x, //
+
+max_v, min_v = -float('inf'), float('inf')
+dfs(1, A[0], op)
+print(max_v)
+print(min_v)
