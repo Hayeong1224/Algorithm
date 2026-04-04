169956e8fc7fd69aa71d1c1f8c2f1bcb471d775b
Hayoung <76741020+Hayeong1224@users.noreply.github.com>
2026-04-05 01:14:14 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 0.17 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
@@ -1,52 +1,37 @@
-#include<bits/stdc++.h>
+#include<vector>
+#include<queue>
+
 using namespace std;
 
-int dy[4] = {1,0,-1,0};
-int dx[4] = {0,1,0,-1};
-bool visited[100][100];
-int dist[100][100];  // 각 좌표까지의 최단 거리를 저장
-int N, M;
-int board[100][100];
+struct Pos {
+    int x, y;
+};
 
-bool canGo(int y, int x){
-    return y < N && 0 <= y && x < M && 0 <= x && (board[y][x] == 1);
-}
+int dx[] = {0,0,1,-1};
+int dy[] = {1,-1,0,0};
 
-int bfs(int y, int x){
-    queue<pair<int,int>> q;
-    q.push(make_pair(y, x));
-    visited[y][x] = true;
-    dist[y][x] = 1;  // 시작점의 거리는 1로 초기화
-
-    while(!q.empty()){
-        pair<int,int> cur = q.front();
+int solution(vector<vector<int> > maps)
+{
+    int n = maps.size();
+    int m = maps[0].size();
+    
+    queue<Pos> q;
+    q.push({0,0});
+    
+    while (!q.empty()) {
+        Pos cur = q.front();
         q.pop();
-        for(int k=0; k<4; k++){
-            int ny = cur.first + dy[k], nx = cur.second + dx[k];
-            if(canGo(ny, nx) && !visited[ny][nx]) {
-                q.push(make_pair(ny,nx));
-                visited[ny][nx] = true;
-                dist[ny][nx] = dist[cur.first][cur.second] + 1;  // 이전 칸 거리 + 1
+        
+        for (int i = 0; i < 4; i++) {
+            int nx = cur.x + dx[i];
+            int ny = cur.y + dy[i];
+            
+            if (0 <= nx && nx < n && 0 <= ny && ny < m && maps[nx][ny] == 1) {
+                maps[nx][ny] = maps[cur.x][cur.y] + 1;
+                q.push({nx,ny});
             }
         }
     }
     
-    // (N-1, M-1) 위치에 도착할 수 있으면 해당 위치까지의 최단 거리 반환
-    if(visited[N-1][M-1]) return dist[N-1][M-1];
-    else return -1;
-}
-
-int solution(vector<vector<int>> maps)
-{
-    N = maps.size();
-    M = maps[0].size();
-    
-    // 초기화
-    memset(visited, false, sizeof(visited));
-    memset(dist, 0, sizeof(dist));
-    
-    for(int i=0; i<N; i++)
-        for(int j=0; j<M; j++) board[i][j] = maps[i][j];
-    
-    return bfs(0, 0);
+    return maps[n-1][m-1] > 1 ? maps[n-1][m-1] : -1;
 }
