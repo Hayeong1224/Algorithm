9cd1b21e14285ec8af945cecd3d3a42c91b21b1e
Hayoung <76741020+Hayeong1224@users.noreply.github.com>
2026-04-05 15:53:26 +0900

    [level 3] Title: 단어 변환, Time: 7.96 ms, Memory: 9.28 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,22 +1,24 @@
 from collections import deque
 
 def is_enable_to_change(a, b):
-    cnt = 0
-    for c1, c2 in zip(a,b):
-        if c1 != c2:
-            cnt += 1
-    return True if cnt == 1 else False
+    return sum (1 for c1, c2 in zip(a,b) if c1 != c2) == 1
 
 def solution(begin, target, words):
+    if target not in words:
+        return 0
+    
     q = deque([(begin, 0)])
+    visited = set([begin])
+    
     while q:
         cur, dist = q.popleft()
+        
         if cur == target:
             return dist
         
-        for i in range(len(words)):
-            w = words[i]
-            if is_enable_to_change(cur, w):
-                words[i] = ''
-                q.append((w, dist+1))
+        for w in words:
+            if is_enable_to_change(cur,w):
+                visited.add(w)
+                q.append((w, dist + 1))
+                
     return 0
