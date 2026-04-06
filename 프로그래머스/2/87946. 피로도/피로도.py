875182699ca098580ef28aca35bbbcd10c2ff3c4
Hayoung <76741020+Hayeong1224@users.noreply.github.com>
2026-04-06 19:15:19 +0900

    [level 2] Title: 피로도, Time: 50.56 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,13 +1,16 @@
 from itertools import permutations
+
 def solution(k, dungeons):
-    max_count = 0
-    perms = permutations(dungeons,len(dungeons))
-    for p in perms:
-        temp_count = 0
-        temp_k = k
-        for required, consumed  in p:
-            if temp_k >= required:
-                temp_k -= consumed
-                temp_count += 1
-        max_count = max(max_count, temp_count)
-    return(max_count)
+    for i in range(len(dungeons), 0, -1):
+        for p in permutations(dungeons, i):
+            cur_k = k
+            success = True
+            
+            for need, spend in p:
+                if cur_k >= need and cur_k - spend >= 0:
+                    cur_k -= spend
+                else: 
+                    success = False
+                    break
+            
+            if success: return i
