2103acf3e1f938b67df052b15a09438d9d494407
Hayoung <76741020+Hayeong1224@users.noreply.github.com>
2026-04-09 01:48:26 +0900

    [level 3] Title: N으로 표현, Time: 14.56 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.py
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.py
@@ -1,20 +1,21 @@
 def solution(N, number):
+    dp = [set() for i in range(9)]
     
-    d = [set() for _ in range(9) ]
-    
-    for i in range(1,9):
-        d[i].add(int(str(N)*i))
-        
-        for j in range(1,i):
-            for op1 in d[j]:
-                for op2 in d[i-j]:
-                    d[i].add(op1 + op2)
-                    d[i].add(op1 - op2)
-                    d[i].add(op1 * op2)
+    for i in range(1, 9): # N의 개수
+        dp[i].add(int(str(N)*  i))
+        for j in range(i//2 + 1):
+            for op1 in dp[j]: # N을 j번 사용해서 만들 수 있는 숫자들
+                for op2 in dp[i-j]: # N을 i - j번 사용해서 만들 수 있는 숫자들
+                    dp[i].add(op1 + op2)
+                    dp[i].add(op1 - op2)
+                    dp[i].add(op2 - op1)
+                    dp[i].add(op1 * op2)
                     if op2 != 0:
-                        d[i].add(op1 // op2)
+                        dp[i].add(op1 // op2)
+                    if op1 != 0:
+                        dp[i].add(op2 // op1)
                         
-        if number in d[i]:
+        if number in dp[i]:
             return i
-        
+    
     return -1
