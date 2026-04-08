035fbec3c3a919a3921dbc73cdb30d5731f17daf
Hayoung <76741020+Hayeong1224@users.noreply.github.com>
2026-04-08 18:44:16 +0900

    [level 3] Title: 순위, Time: 82.10 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/README.md
+++ 프로그래머스/3/49191. 순위/README.md
@@ -1,10 +1,10 @@
 # [level 3] 순위 - 49191 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49191?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49191?language=python3) 
 
 ### 성능 요약
 
-메모리: 4.47 MB, 시간: 6.29 ms
+메모리: 10.1 MB, 시간: 82.10 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 08일 18:22:15
+2026년 04월 08일 18:44:14
 
 ### 문제 설명
 
--- 프로그래머스/3/49191. 순위/순위.py
+++ 프로그래머스/3/49191. 순위/순위.py
@@ -2,21 +2,15 @@ def solution(n, results):
     win = [set() for _ in range(n+1)]
     lose = [set() for _ in range(n+1)]
     
-    for a, b in results:
-        win[a].add(b)
-        lose[b].add(a)
+    for winner, loser in results:
+        win[winner].add(loser)
+        lose[loser].add(winner)
         
-    for i in range(1,n+1):
-        # 나에게 진 사람들은 내가 진 사람들한테도 진 거
-        for loser in win[i]:
-            lose[loser].update(lose[i])
-        # 나를 이긴 사람들은 내가 이긴 사람들도 이긴 거
-        for winner in lose[i]:
-            win[winner].update(win[i])
-            
-    answer = 0
-    for i in range(1, n+1):
-        if len(win[i]) + len(lose[i]) == n - 1:
-            answer += 1
-            
-    return answer
+    for k in range(1, n+1):
+        for i in range(1, n+1):
+            for j in range(1, n+1):
+                if k in win[i] and j in win[k]:
+                    win[i].add(j)
+                    lose[j].add(i)
+    
+    return sum(1 for i in range(1, n+1) if len(win[i]) + len(lose[i]) == n-1)
