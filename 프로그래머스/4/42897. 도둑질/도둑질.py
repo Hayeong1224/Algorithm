31109840110262809bad1e167f180264ea0fcac2
Hayoung <76741020+Hayeong1224@users.noreply.github.com>
2026-04-09 04:40:52 +0900

    [level 4] Title: 도둑질, Time: 598.81 ms, Memory: 89.5 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.py
+++ 프로그래머스/4/42897. 도둑질/도둑질.py
@@ -1,20 +1,17 @@
 def solution(money):
-    n = len(money)
-    if n == 1: return money[0]
-
-    # choose first -> ignore last
-    dp_f = [0] * n
-    dp_f[0] = money[0]
-    dp_f[1] = max(money[0], money[1])
     
-    for i in range(2, n-1):
-        dp_f[i] = max(dp_f[i-2] + money[i], dp_f[i-1])
+    # 첫 집 털기, 마지막 집은 못 털어!
+    dp1 = [0] * len(money)
+    dp1[0] = money[0]
+    dp1[1] = dp1[0]
+    for i in range(2, len(money)-1):
+        dp1[i] = max(dp1[i-2] + money[i], dp1[i-1])
+    
+    # 첫 집 안 털기, 마지막 집 털기
+    dp2 = [0] * len(money)
+    dp2[0] = 0
+    dp2[1] = money[1]
+    for i in range(2, len(money)):
+        dp2[i] = max(dp2[i-2] + money[i], dp2[i-1])
         
-    # choose last -> ignore first
-    dp_l = [0] * n
-    dp_l[1] = money[1]
-    for i in range(2, n):
-        dp_l[i] = max(dp_l[i-2] + money[i], dp_l[i-1])
-    
-    
-    return max(max(dp_f), max(dp_l))
+    return max(max(dp1), max(dp2))
