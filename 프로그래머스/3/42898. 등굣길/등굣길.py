a9ef1858f771ed6c4e85d131a5d5d7f6bfd22351
Hayoung <76741020+Hayeong1224@users.noreply.github.com>
2026-04-09 02:53:17 +0900

    [level 3] Title: 등굣길, Time: 1.94 ms, Memory: 9.4 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.py
+++ 프로그래머스/3/42898. 등굣길/등굣길.py
@@ -1,15 +1,23 @@
 def solution(m, n, puddles):
-    d = [[0] * (m+1) for _ in range(n+1)]
-    d[1][1] = 1
+    # (1,1) -> (m,n)
+    # 오, 아
+    dp = [[0] * (m+1) for _ in range(n+1)]
+    dp[1][1] = 1
+    for y, x in puddles:
+        dp[x][y] = -1
     
-    puddle_set = set((y, x) for x, y in puddles)
-    
+    # 집에서 학교까지 갈 수 있는 최단경로의 개수
     for i in range(1, n+1):
         for j in range(1, m+1):
-            if (i, j) == (1, 1): continue
-            if (i, j) in puddle_set:
-                d[i][j] = 0
-            else:
-                d[i][j] = (d[i][j-1] + d[i-1][j]) % 1000000007
+            if i == 1 and j == 1:
+                continue
+            
+            if dp[i][j] == -1:
+                dp[i][j] = 0
+                continue
+            
+            up = dp[i-1][j]
+            left = dp[i][j-1]
+            dp[i][j] = (up + left) % 1000000007
     
-    return d[n][m]
+    return dp[n][m]
