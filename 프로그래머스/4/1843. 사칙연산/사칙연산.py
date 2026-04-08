d1538b7ece0832a66fc2d7914220fb91e0b5a172
Hayoung <76741020+Hayeong1224@users.noreply.github.com>
2026-04-09 03:30:31 +0900

    [level 4] Title: 사칙연산, Time: 117.74 ms, Memory: 9.56 MB -BaekjoonHub

--- 프로그래머스/4/1843. 사칙연산/사칙연산.py
+++ 프로그래머스/4/1843. 사칙연산/사칙연산.py
@@ -1,23 +1,26 @@
+INF = 1e9
 def solution(arr):
+    # 덧셈 뒤에 나오는 수는 가장 커져야 하고, 뺄셈 뒤에 나오는 수는 가장 작아져야 함 -> 최대값과 최솟값 계산
     nums = [int(x) for x in arr[::2]]
     ops = arr[1::2]
-    n = len(nums)
+    N = len(nums)
     
-    max_d = [[-float('inf')] * n for _ in range(n)]
-    min_d = [[float('inf')] * n for _ in range(n)]
+    min_dp = [[INF] * N for i in range(N)]
+    max_dp = [[-INF] * N for i in range(N)]
     
-    for i in range(n):
-        max_d[i][i] = min_d[i][i] = nums[i]
+    for i in range(N):
+        min_dp[i][i] = max_dp[i][i] = nums[i]
     
-    for length in range(1, n):
-        for i in range(n - length):
-            j = i + length
-            for k in range(i,j):
+    for size in range(1, N): # 부분배열 크기
+        for i in range(N - size): # 시작점
+            j = i + size # 끝점
+            for k in range(i, j):
                 if ops[k] == '+':
-                    max_d[i][j] = max(max_d[i][j], max_d[i][k] + max_d[k+1][j])
-                    min_d[i][j] = min(min_d[i][j], min_d[i][k] + min_d[k+1][j])
+                    max_dp[i][j] = max(max_dp[i][j], max_dp[i][k] + max_dp[k+1][j])
+                    min_dp[i][j] = min(min_dp[i][j], min_dp[i][k] + min_dp[k+1][j])
                 else:
-                    max_d[i][j] = max(max_d[i][j], max_d[i][k] - min_d[k+1][j])
-                    min_d[i][j] = min(min_d[i][j], min_d[i][k] - max_d[k+1][j])
+                    max_dp[i][j] = max(max_dp[i][j], max_dp[i][k] - min_dp[k+1][j])
+                    min_dp[i][j] = min(min_dp[i][j], min_dp[i][k] - max_dp[k+1][j])
+                    
+    return max_dp[0][-1]
     
-    return max_d[0][n-1]
