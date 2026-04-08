09d357bc2265465bee4e3a0f79f5398abf89d16a
Hayoung <76741020+Hayeong1224@users.noreply.github.com>
2026-04-08 19:16:39 +0900

    [level 5] Title: 방의 개수, Time: 355.34 ms, Memory: 48.1 MB -BaekjoonHub

--- 프로그래머스/5/49190. 방의 개수/README.md
+++ 프로그래머스/5/49190. 방의 개수/README.md
@@ -1,10 +1,10 @@
 # [level 5] 방의 개수 - 49190 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49190?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49190) 
 
 ### 성능 요약
 
-메모리: 59.2 MB, 시간: 285.21 ms
+메모리: 48.1 MB, 시간: 355.34 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 01월 11일 13:53:30
+2026년 04월 08일 19:16:36
 
 ### 문제 설명
 
--- 프로그래머스/5/49190. 방의 개수/방의 개수.py
+++ 프로그래머스/5/49190. 방의 개수/방의 개수.py
@@ -1,22 +1,29 @@
 def solution(arrows):
-    move = [(-1,0), (-1,1), (0,1), (1,1), (1,0), (1,-1), (0,-1), (-1,-1)]
-    now = (0, 0)
+    dx = [-1,-1,0,1,1,1,0,-1]
+    dy = [0,1,1,1,0,-1,-1,-1]
+    
+    # 방 생성 기준? 선으로 둘러 싸임 - 다른 방향을 또 들어오면 방 하나?
+    now = (0,0)
     visited_nodes = {now}
     visited_edges = set()
-
-    count = 0
     
-    for arrow in arrows:
-        for _ in range(2): # 대각선 교차 예외 문제 해결을 위해 2배로 이동
-            next_node = (now[0] + move[arrow][0], now[1]+ move[arrow][1])
-            edge = tuple(sorted([now, next_node])) # 경로는 방향이 없으므로 정렬해서 저장
+    ans = 0
+    for i in arrows:
+        # 두 칸 이동
+        for _ in range(2):
+            nxt_node = (now[0] + dx[i], now[1] + dy[i])
+            
+            # 방이 생성되는 조건 확인
+            if nxt_node in visited_nodes:
+                # 이미 방문한 노드인데, 처음 가는 경로면 방 + 1
+                if (now, nxt_node) not in visited_edges:
+                    ans += 1
+                    
+            visited_nodes.add(nxt_node)
+            # 간선 양방향 저장
+            visited_edges.add((now, nxt_node))
+            visited_edges.add((nxt_node, now))
+            
+            now = nxt_node
         
-            # 처음 가보는 경로인데, 이미 방문한 노드일 때 방 생성
-            if next_node in visited_nodes and edge not in visited_edges:
-                count += 1
-        
-            visited_nodes.add(next_node)
-            visited_edges.add(edge)
-            now = next_node
-        
-    return count
+    return ans
