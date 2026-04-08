097cf8351ea1b6718bdf88404eb6330d25c7304c
Hayoung <76741020+Hayeong1224@users.noreply.github.com>
2026-04-09 04:05:39 +0900

    [level 4] Title: 사칙연산, Time: 117.64 ms, Memory: 9.49 MB -BaekjoonHub

--- 프로그래머스/4/1843. 사칙연산/사칙연산.py
+++ 프로그래머스/4/1843. 사칙연산/사칙연산.py
@@ -1,20 +1,21 @@
-INF = 1e9
 def solution(arr):
     # 덧셈 뒤에 나오는 수는 가장 커져야 하고, 뺄셈 뒤에 나오는 수는 가장 작아져야 함 -> 최대값과 최솟값 계산
     nums = [int(x) for x in arr[::2]]
     ops = arr[1::2]
     N = len(nums)
     
-    min_dp = [[INF] * N for i in range(N)]
-    max_dp = [[-INF] * N for i in range(N)]
+    # dp[i][j] : i번째 숫자부터 j번째 숫자까지 연산 결과
+    min_dp = [[float('inf')] * N for i in range(N)]
+    max_dp = [[-float('inf')] * N for i in range(N)]
     
     for i in range(N):
         min_dp[i][i] = max_dp[i][i] = nums[i]
     
-    for size in range(1, N): # 부분배열 크기
+    # 구간의 길이(size)를 1무터 N-1까지 늘려가며 확인
+    for size in range(1, N):
         for i in range(N - size): # 시작점
             j = i + size # 끝점
-            for k in range(i, j):
+            for k in range(i, j): # 괄호 나누는 지점 선택
                 if ops[k] == '+':
                     max_dp[i][j] = max(max_dp[i][j], max_dp[i][k] + max_dp[k+1][j])
                     min_dp[i][j] = min(min_dp[i][j], min_dp[i][k] + min_dp[k+1][j])
@@ -22,5 +23,4 @@ def solution(arr):
                     max_dp[i][j] = max(max_dp[i][j], max_dp[i][k] - min_dp[k+1][j])
                     min_dp[i][j] = min(min_dp[i][j], min_dp[i][k] - max_dp[k+1][j])
                     
-    return max_dp[0][-1]
-    
+    return max_dp[0][-1] # 0부터 마지막까지 계산한 최댓값
