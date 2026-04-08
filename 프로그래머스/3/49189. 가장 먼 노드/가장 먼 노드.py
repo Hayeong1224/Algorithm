bebcd797e484dda4efd55536d12f01e37b666039
Hayoung <76741020+Hayeong1224@users.noreply.github.com>
2026-04-08 17:19:19 +0900

    [level 3] Title: 가장 먼 노드, Time: 32.69 ms, Memory: 19.8 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -1,23 +1,32 @@
 from collections import deque
 
-
 def solution(n, edge):
-    graph = [[] for _ in range(n+1)]
+    # 간선 길이가 동일 -> BFS 해도 되겠다!
+    # 인접 리스트부터 구하기!
+    adj = [[] * (n+1) for _ in range(n+1)]
+    visited = [False] * (n+1)
+    for n1, n2 in edge:
+        adj[n1].append(n2)
+        adj[n2].append(n1)
     
-    for a, b in edge:
-        graph[a].append(b)
-        graph[b].append(a)
-    
-    distance = [-1] * (n+1)
-    distance[1] = 0
-    
-    q = deque([1])
+    # BFS - 1번 노드 시작, (노드, dist) 넣어서 maxDist를 갱신하면 될 듯!
+    maxDist = 0
+    maxNodeCnt = 0  
+    q = deque([(1,0)])
+    visited[1] = True
     
     while q:
-        now = q.popleft()
-        for v in graph[now]:
-            if distance[v] == -1:
-                distance[v] = distance[now] + 1
-                q.append(v)
-                
-    return distance.count(max(distance))
+        cn, dist = q.popleft()
+
+        if maxDist < dist:
+            maxDist = dist
+            maxNodeCnt = 1
+        elif maxDist == dist:
+            maxNodeCnt += 1
+        
+        for n in adj[cn]:
+            if not visited[n]:
+                q.append((n, dist + 1))
+                visited[n] = True
+        
+    return maxNodeCnt
