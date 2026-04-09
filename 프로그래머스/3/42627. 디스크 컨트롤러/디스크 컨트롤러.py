19b7175c1a0f7c8ea7e755e278d96dfea5c0603a
Hayoung <76741020+Hayeong1224@users.noreply.github.com>
2026-04-09 18:26:22 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 15.33 ms, Memory: 9.29 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,25 +1,32 @@
 import heapq
+
 def solution(jobs):
-    ans, now, i = 0, 0, 0
+    pq = []
+    now, n = 0, len(jobs)
+    i = 0 # 처리한 job의 수
     start = -1
-    waiting = []
+    total_return_time = 0 # 반환 시간
     
-    while i < len(jobs):
-        for j in jobs:
-            if start < j[0] <= now:
-                heapq.heappush(waiting, [j[1], j[0]])
+    indexed_jobs = []
+    for idx, job in enumerate(jobs):
+        indexed_jobs.append([job[0], job[1], idx])
+    indexed_jobs.sort()
+    
+    
+    while i < n:        
+        # 소요시간이 짧은 것, 요청 시각이 빠른 것, 작업의 번호가 작은 것 순으로 우선순위 높음
+        for j_ask_time, j_term_time, j_idx in indexed_jobs:
+            if start < j_ask_time <= now:
+                heapq.heappush(pq, (j_term_time, j_ask_time, j_idx))
+            elif j_ask_time > now: break
         
-        if waiting:
-            cur = heapq.heappop(waiting)
+        if len(pq) > 0: # 작업 시작
+            term_time, ask_time, _ = heapq.heappop(pq)
             start = now
-            now += cur[0]
-            ans += now - cur[1]
+            now += term_time
+            total_return_time += (now - ask_time)
             i += 1
-        else:
-            now += 1
-            
-    return ans // len(jobs)
-            
-                
-                
+        else: # 다음 작업 시간으로 점프
+            now = indexed_jobs[i][0]
         
+    return total_return_time // n
