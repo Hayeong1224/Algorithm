d85605ba90bdba10811d446110620ccc162bcfaf
Hayoung <76741020+Hayeong1224@users.noreply.github.com>
2026-04-09 16:49:05 +0900

    [level 3] Title: 입국심사, Time: 749.23 ms, Memory: 13.5 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,16 +1,22 @@
 def solution(n, times):
-    start = min(times)
-    end = max(times) * n
+    # 모든 사람이 심사 받는데 걸리는 시간의 최솟값
+    # (이분 탐색): T라는 시간이 주어졌을 때, 모든 심사관이 최대 몇 명을 심사할 수 있는가?
     answer = 0
-    while start <= end:
-        sum = 0
-        mid = (start + end) // 2
-        for x in times:
-            sum += mid // x
-        if sum < n :
-            start = mid + 1
-        else:
-            answer = mid
-            end = mid - 1
+    times.sort();
+    
+    low, high = 0, times[-1] * n
+    while low <= high:
+        mid = (low + high) // 2
+        total = 0
+        
+        for time in times:
+            total += mid // time
+            if total >= n: break
             
-    return answer
+        if total >= n:
+            answer = mid # n명 이상이면 정답 후보
+            high = mid - 1
+        else:
+            low = mid + 1
+            
+    return answer 
