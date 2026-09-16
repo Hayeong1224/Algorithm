d1830c770d7d1afc23c3635285e95fd0033b33ce
Hayoung <76741020+Hayeong1224@users.noreply.github.com>
2026-09-16 19:35:40 +0900

    [level 2] Title: 올바른 괄호, Time: 5.94 ms, Memory: 11.8 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/README.md
+++ 프로그래머스/2/12909. 올바른 괄호/README.md
@@ -1,10 +1,10 @@
 # [level 2] 올바른 괄호 - 12909 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909) 
 
 ### 성능 요약
 
-메모리: 9.29 MB, 시간: 6.16 ms
+메모리: 11.8 MB, 시간: 5.94 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 05일 18:21:50
+2026년 09월 16일 19:35:36
 
 ### 문제 설명
 
--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,16 +1,12 @@
-from collections import deque
-
 def solution(s):
-    q = deque()
-    for b in s:
-        if b == '(':
-            q.append(b)
-        else:
-            if not q:
+    stack = []
+    for cur in s:
+        if cur == ')':
+            if stack and stack[-1] == '(':
+                stack.pop()
+            else:
                 return False
-            else: # '('가 있다면
-                q.pop()
-    
-    if q:
-        return False
-    return True
+        else:
+            stack.append('(')
+                
+    return True if len(stack) == 0 else False # stack 비어있으면 true
