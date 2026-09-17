7880bd6686782ff1d7a5cde3275dcb52ba75b3c6
Hayoung <76741020+Hayeong1224@users.noreply.github.com>
2026-09-17 21:41:55 +0900

    [level 1] Title: 최소직사각형, Time: 0.72 ms, Memory: 12.7 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -1,9 +1,10 @@
 def solution(sizes):
-    # 둘 중 큰 걸 한 쪽으로 몰아버리자!
-    mw, mh = 0, 0
+    # 가로, 세로 중 큰 값을 가로로 몰고 작은 값을 세로로 몰아서 최대값 구하기
+    w_max, h_max = 0, 0
     for w, h in sizes:
-        temp_w = max(w, h)
-        temp_h = min(w, h)
-        mw = max(mw, temp_w)
-        mh = max(mh, temp_h)
-    return mw * mh 
+        if w < h:
+            w, h = h, w
+        w_max = max(w_max, w)
+        h_max = max(h_max, h)
+        
+    return w_max * h_max
