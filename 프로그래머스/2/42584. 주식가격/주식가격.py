077bc64faca606f7e77f3f4641da62c6a494f3e3
Hayoung <76741020+Hayeong1224@users.noreply.github.com>
2026-09-17 17:09:36 +0900

    [level 2] Title: 주식가격, Time: 89.76 ms, Memory: 16.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,19 +1,18 @@
 def solution(prices):
-    n = len(prices)
-    answer = [0] * n
-    stack = []
+    answer = [0] * len(prices)
     
-    for i in range(n):
-        # 가격이 떨어졌다면, 그 전의 모든 가격들을 POP하며 시간 계산
-        while stack and stack[-1][0] > prices[i]:
-            price, idx = stack.pop()
-            answer[idx] = i - idx
-            
-        stack.append((prices[i], i))
+    for i in range(len(prices)):
+        time = 0
         
-    # 끝까지 가격이 떨어지지 않은 나머지
-    while stack:
-        price, idx = stack.pop()
-        answer[idx] = (n-1) - idx
-    
+        for j in range(i+1, len(prices)):
+            if prices[i] <= prices[j]:
+                time += 1
+            else:
+                time += 1 # 다음까지 떨어지진 않았으니 +1 & 더 이상 측정 x
+                break
+        
+        answer[i] = time
+        
     return answer
+
+# 가격이 떨어지지 않은 기간은 몇 초인지
