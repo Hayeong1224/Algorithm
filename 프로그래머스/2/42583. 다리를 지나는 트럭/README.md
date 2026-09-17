0be539e1f27f0f0144ca655afb9c84de92a31798
Hayoung <76741020+Hayeong1224@users.noreply.github.com>
2026-09-17 16:52:26 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 20.98 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/README.md
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/README.md
@@ -1,10 +1,10 @@
 # [level 2] 다리를 지나는 트럭 - 42583 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583) 
 
 ### 성능 요약
 
-메모리: 9.09 MB, 시간: 52.24 ms
+메모리: 11.5 MB, 시간: 20.98 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 05월 08일 00:47:28
+2026년 09월 17일 16:52:23
 
 ### 문제 설명
 
--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -1,22 +1,25 @@
 from collections import deque
+
 def solution(bridge_length, weight, truck_weights):
-    answer = 0 # 현재 시간
-    q = deque() # 다리 위 트럭 (무게, 나갈 시간)
-    nxt = 0 # 다음 트럭
-    cur_weights = 0 # 현재 다리 위 트럭 총 무게
+    waiting = deque(truck_weights)
+    bridge = deque([0] * bridge_length)
     
-    while nxt < len(truck_weights) or q:
-        answer += 1
+    time = 0
+    cur_weight = 0
+    
+    while bridge:
+        time += 1
         
-        # 트럭 나가야 하는지 확인
-        if q and q[0][1] == answer:
-            w, t = q.popleft()
-            cur_weights -= w
+        # 제일 앞 트럭 빠져나감
+        cur_weight -= bridge.popleft()
+        
+        # 새 트럭 투입
+        if waiting:
+            if cur_weight + waiting[0] <= weight:
+                truck = waiting.popleft()
+                bridge.append(truck)
+                cur_weight += truck
+            else:
+                bridge.append(0)
             
-        # 새로운 트럭: 다리 건너기
-        if nxt < len(truck_weights) and cur_weights + truck_weights[nxt] <= weight:
-            q.append((truck_weights[nxt], answer + bridge_length))
-            cur_weights += truck_weights[nxt]
-            nxt += 1
-    
-    return answer
+    return time
