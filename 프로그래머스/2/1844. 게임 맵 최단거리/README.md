4100aee23006a09e77b90a6fca922d281cab4c52
Hayoung <76741020+Hayeong1224@users.noreply.github.com>
2026-09-17 17:59:44 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 10.64 ms, Memory: 12.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
 
 ### 성능 요약
 
-메모리: 4.14 MB, 시간: 0.17 ms
+메모리: 12.4 MB, 시간: 10.64 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 05일 01:14:13
+2026년 09월 17일 17:59:41
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,25 +1,24 @@
 from collections import deque
 
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
+
 def solution(maps):
-    dx = [0, 1, 0, -1]
-    dy = [-1, 0, 1, 0]
-    
     n, m = len(maps), len(maps[0])
     
-    visited = [[False] * m for _ in range(n)]
-    q = deque([(0,0,1)])
-    visited[0][0] = True
+    q = deque([(0, 0, 1)])
+    visited = set()
+    visited.add((0,0))
+    
     while q:
         cx, cy, dist = q.popleft()
-        
         if cx == n-1 and cy == m-1:
             return dist
         
         for i in range(4):
-            nx = cx + dx[i]
-            ny = cy + dy[i]
-            if 0<=nx<n and 0<=ny<m and maps[nx][ny] == 1 and not visited[nx][ny]:
+            nx, ny = cx + dx[i], cy + dy[i]
+            if 0<=nx<n and 0<=ny<m and maps[nx][ny] and (nx,ny) not in visited:
+                visited.add((nx,ny))
                 q.append((nx,ny,dist+1))
-                visited[nx][ny] = True
-                
+        
     return -1
