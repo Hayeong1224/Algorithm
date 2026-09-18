50859c01d01435403f3fea536ce44c54cb4e4559
Hayoung <76741020+Hayeong1224@users.noreply.github.com>
2026-09-19 02:08:26 +0900

    [level 3] Title: N으로 표현, Time: 6.57 ms, Memory: 12.5 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.py
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.py
@@ -1,21 +1,21 @@
 def solution(N, number):
-    dp = [set() for i in range(9)]
+    # DP[값]이 아니라 DP[N 사용 개수]
+    dp = [set() for _ in range(9)]
     
-    for i in range(1, 9): # N의 개수
-        dp[i].add(int(str(N)*  i))
-        for j in range(i//2 + 1):
-            for op1 in dp[j]: # N을 j번 사용해서 만들 수 있는 숫자들
-                for op2 in dp[i-j]: # N을 i - j번 사용해서 만들 수 있는 숫자들
-                    dp[i].add(op1 + op2)
-                    dp[i].add(op1 - op2)
-                    dp[i].add(op2 - op1)
-                    dp[i].add(op1 * op2)
-                    if op2 != 0:
-                        dp[i].add(op1 // op2)
-                    if op1 != 0:
-                        dp[i].add(op2 // op1)
-                        
+    for i in range(1, 9): # 최솟값이 8
+        # N, NN, NNN, ... 
+        dp[i].add(int(str(N)*i))
+        
+        # j개 사용한 결과 + (i-j)개 사용한 결과
+        for j in range(1, i):
+            for a in dp[j]:
+                for b in dp[i-j]:
+                    dp[i].add(a+b)
+                    dp[i].add(a-b)
+                    dp[i].add(a*b)
+                    if b != 0:
+                        dp[i].add(a // b)
+    
         if number in dp[i]:
             return i
-    
     return -1
--- 프로그래머스/3/42895. N으로 표현/README.md
+++ 프로그래머스/3/42895. N으로 표현/README.md
@@ -1,10 +1,10 @@
 # [level 3] N으로 표현 - 42895 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42895?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42895) 
 
 ### 성능 요약
 
-메모리: 4.32 MB, 시간: 9.33 ms
+메모리: 12.5 MB, 시간: 6.57 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 09일 02:06:35
+2026년 09월 19일 02:08:23
 
 ### 문제 설명
 
